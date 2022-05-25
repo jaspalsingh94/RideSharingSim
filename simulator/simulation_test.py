e501f11ece203c81e83d1f43fa4d587258524bad
Youngsu Jung <terun707@naver.com>
2022-05-25 16:17:42 -0400

    modified test file
    use copied list for input

--- simulator/simulation_test.py
+++ simulator/simulation_test.py
@@ -3,14 +3,14 @@ from generate_customers import generate_customer_list
 
 customers = generate_customer_list(size=100)
 
-result = simulation_test(customers, customer_limit=1, wait_limit=900, VEHICLE_NUM_PER_ZONE=1, printB=False)
+result = simulation_test(list(customers), customer_limit=1, wait_limit=900, VEHICLE_NUM_PER_ZONE=1, printB=False)
 if result: print(result)
 
-result = simulation_test(customers, customer_limit=1, wait_limit=900, VEHICLE_NUM_PER_ZONE=4, printB=False)
+result = simulation_test(list(customers), customer_limit=1, wait_limit=900, VEHICLE_NUM_PER_ZONE=4, printB=False)
 if result: print(result)
 
-result = simulation_test(customers, customer_limit=5, wait_limit=900, VEHICLE_NUM_PER_ZONE=1, printB=False)
+result = simulation_test(list(customers), customer_limit=5, wait_limit=900, VEHICLE_NUM_PER_ZONE=1, printB=False)
 if result: print(result)
 
-result = simulation_test(customers, customer_limit=5, wait_limit=900, VEHICLE_NUM_PER_ZONE=4, printB=False)
+result = simulation_test(list(customers), customer_limit=5, wait_limit=900, VEHICLE_NUM_PER_ZONE=4, printB=False)
 if result: print(result)
